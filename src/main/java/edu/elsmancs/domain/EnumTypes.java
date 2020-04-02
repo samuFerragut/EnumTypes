3863c64aea156694abe5082cebae586124420732
samuFerragut <sferragut@cifpfbmoll.eu>
2020-04-02 14:13:04 +0200

    feat(EnumTypes): Añadir lógica main

--- src/main/java/edu/elsmancs/domain/EnumTypes.java
+++ src/main/java/edu/elsmancs/domain/EnumTypes.java
@@ -3,5 +3,23 @@ package edu.elsmancs.domain;
 import edu.elsmancs.domain.Planeta;
 
 public class EnumTypes {
+    public static void main(String[] args) {
+
+        double peso = 175.0; // kg
+
+        for(Planeta planeta : Planeta.values()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Planeta planeta: Planeta.getGigantesGaseosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+    }
 
 }
